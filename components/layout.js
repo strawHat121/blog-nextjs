7e405ec9993983bbbf5235dbde046a6089b99ac8
Kinjalk <kinjalkbajpai@gmail.com>
2023-04-16 00:58:06 +0530

    Hosting the blog website

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Kinjalk'
+export const siteTitle = 'Strawhat\'s Blogsss'
 
 export default function Layout({ children, home }) {
   return (
---
+++ posts/first-blog.md
@@ -0,0 +1,6 @@
+---
+title: "First Blog"
+date: "2023-04-15"
+---
+
+Made this site to render all of mine Blogs written in Markdown.
